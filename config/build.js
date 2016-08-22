7d5dac2a7d6b017e39004727a9aae4bb4edf7ddf
Sam Keller <skellertor@gmail.com>
2016-08-22 01:33:35 -0600

    commit

--- config/build.js
+++ config/build.js
@@ -1,10 +1,20 @@
+// app.directive('htmlSrc', function () {
+//   return {
+//     restrict: 'A',
+//     link: function (scope, element, attrs) {
+//       scope.$watch(attrs.htmlSrc, function (value) {
+//         attrs.$set('src', 'data:text/html,' + value);
+//       });
+//     }
+//   };
+// });
 app.directive('htmlSrc', function () {
   return {
     restrict: 'A',
     link: function (scope, element, attrs) {
-      scope.$watch(attrs.htmlSrc, function (value) {
+      scope.$watch('data.html', function (value) {
         attrs.$set('src', 'data:text/html,' + value);
-      });
+      },true);
     }
   };
 });
